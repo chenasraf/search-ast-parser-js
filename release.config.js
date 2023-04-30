416fee3ec14267b22812fd7132147831c1a8fcd2
Chen Asraf <chenasrafil@gmail.com>
2023-05-01 00:22:08 +0300

    build: fix build

--- release.config.js
+++ release.config.js
@@ -1,75 +1,75 @@
 const releaseRules = [
-  { type: "feat", section: "Features", release: "minor" },
-  { type: "docs", section: "Build", release: false },
-  { type: "fix", section: "Bug Fixes", release: "patch" },
-  { type: "refactor", section: "Misc", release: "patch" },
-  { type: "perf", section: "Misc", release: "patch" },
-  { type: "build", section: "Build", release: "patch" },
-  { type: "chore", section: "Misc", release: "patch" },
-  { type: "test", section: "Tests", release: "patch" },
+  { type: 'feat', section: 'Features', release: 'minor' },
+  { type: 'docs', section: 'Build', release: false },
+  { type: 'fix', section: 'Bug Fixes', release: 'patch' },
+  { type: 'refactor', section: 'Misc', release: 'patch' },
+  { type: 'perf', section: 'Misc', release: 'patch' },
+  { type: 'build', section: 'Build', release: 'patch' },
+  { type: 'chore', section: 'Misc', release: 'patch' },
+  { type: 'test', section: 'Tests', release: 'patch' },
 ]
 
 /** @type {import('semantic-release').Options} */
 module.exports = {
   branches: [
-    "+([0-9])?(.{+([0-9]),x}).x",
-    "master",
-    "next",
-    "next-major",
-    { name: "develop", prerelease: true },
-    { name: "beta", prerelease: true },
-    { name: "alpha", prerelease: true },
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    'next',
+    'next-major',
+    { name: 'develop', prerelease: true },
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
   ],
   analyzeCommits: {
-    path: "semantic-release-conventional-commits",
-    majorTypes: ["major", "breaking"],
-    minorTypes: ["minor", "feat", "feature"],
-    patchTypes: ["patch", "fix", "bugfix", "refactor", "perf", "revert"],
+    path: 'semantic-release-conventional-commits',
+    majorTypes: ['major', 'breaking'],
+    minorTypes: ['minor', 'feat', 'feature'],
+    patchTypes: ['patch', 'fix', 'bugfix', 'refactor', 'perf', 'revert'],
   },
   plugins: [
     [
-      "@semantic-release/commit-analyzer",
+      '@semantic-release/commit-analyzer',
       {
-        preset: "conventionalcommits",
+        preset: 'conventionalcommits',
         parserOpts: {
-          noteKeywords: ["breaking:", "breaking-fix:", "breaking-feat:"],
+          noteKeywords: ['breaking:', 'breaking-fix:', 'breaking-feat:'],
         },
         releaseRules: releaseRules,
       },
     ],
     [
-      "@semantic-release/release-notes-generator",
+      '@semantic-release/release-notes-generator',
       {
-        preset: "conventionalcommits",
+        preset: 'conventionalcommits',
         parserOpts: {
-          noteKeywords: ["breaking"],
+          noteKeywords: ['breaking'],
           types: releaseRules,
         },
       },
     ],
     [
-      "@semantic-release/changelog",
+      '@semantic-release/changelog',
       {
-        changelogFile: "CHANGELOG.md",
-        changelogTitle: "# Change Log",
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle: '# Change Log',
       },
     ],
     [
-      "@semantic-release/npm",
+      '@semantic-release/npm',
       {
-        pkgRoot: "dist",
+        pkgRoot: 'build',
       },
     ],
     [
-      "@semantic-release/git",
+      '@semantic-release/git',
       {
-        assets: ["CHANGELOG.md", "package.json"],
+        assets: ['CHANGELOG.md', 'package.json'],
       },
     ],
     [
-      "@semantic-release/github",
+      '@semantic-release/github',
       {
-        assets: ["package.tgz"],
+        assets: ['package.tgz'],
       },
     ],
   ],
