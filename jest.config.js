e74780dc2ec13c69f165ae939d5a08d9599feb98
Chen Asraf <chenasrafil@gmail.com>
2023-05-01 23:42:41 +0300

    test: clean tests, add coverage + verbose

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
   testMatch: [
     '**/tests/**/*.[jt]s?(x)',
     '**/__tests__/**/*.[jt]s?(x)',
